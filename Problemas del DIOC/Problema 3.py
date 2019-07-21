ee5191ea938115fa1ec1cc2b0b72d50845519062
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-07-20 23:47:40 -0400

    Arreglos al problema 2

--- Problemas del DIOC/Problema 3.py
+++ Problemas del DIOC/Problema 3.py
@@ -3,7 +3,7 @@ diseñe un programa para calcular la ordenada correspondiente a una abscisa x\
 cualquiera (también suministrada como dato) empleando interpolacion lineal."""
 #Le asignamos a las variables las componentes de los puntos P1 y P2.
 #Utilizamos float() para transformar el valor introducido por el usuario en un
-#tipo format (Lo recibido en un input es tipo string)
+#tipo float (Lo recibido en un input es tipo string)
 x1, y1 = float(input("Ingrese las coordenadas del primer punto: \nX: ")),\
 float(input("Y: "))
 x2, y2 = float(input("Ingrese las coordenadas del segundo punto: \nX: ")),\
