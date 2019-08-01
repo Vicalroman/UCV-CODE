5357c8ed6b16ce20ac872cfaa2e098c3bef03ff0
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-07-31 22:18:00 -0400

    Arreglos en el problema 019

--- Problemas del DIOC/Problema 019.py
+++ Problemas del DIOC/Problema 019.py
@@ -2,6 +2,9 @@
 si los con ellos se puede formar un triángulo Acutángulo, Rectángulo o
 Obtusángulo."""
 import math
+def angulosDelTriangulo(a,b,c):
+    angle = math.degrees(math.acos((a**2 + b**2 - c**2)/(2*a*b)))
+    return round(angle)
 def tipoDeTriangulo():
     alpha = angulosDelTriangulo(longitud_1,longitud_2,longitud_3)
     beta = angulosDelTriangulo(longitud_1,longitud_3,longitud_2)
@@ -12,9 +15,6 @@ def tipoDeTriangulo():
         print("Es un triangulo obtuso.")
     elif alpha < 90 and gamma < 90 and beta < 90:
         print("Es un triangulo acutangulo.")
-def angulosDelTriangulo(a,b,c):
-    angle = math.degrees(math.acos((a**2 + b**2 - c**2)/(2*a*b)))
-    return round(angle)
 print("Vamos a identificar triangulos segun la longitud de sus lados.")
 longitud_1 = float(input("Ingresa la longitud del primer lado: "))
 longitud_2 = float(input("Ingresa la longitud del segundo lado: "))
